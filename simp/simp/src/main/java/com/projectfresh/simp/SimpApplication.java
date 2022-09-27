600a52e55ed4535c5a8351b5963134884c7a9f31
CMMH0210 <114383220+CMMH0210@users.noreply.github.com>
2022-09-27 04:38:32 +0000

    Add files via upload

---
+++ simp/simp/src/main/java/com/projectfresh/simp/SimpApplication.java
@@ -0,0 +1,13 @@
+package com.projectfresh.simp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpApplication.class, args);
+	}
+
+}
---
+++ simp/simp/src/test/java/com/projectfresh/simp/SimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectfresh.simp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
